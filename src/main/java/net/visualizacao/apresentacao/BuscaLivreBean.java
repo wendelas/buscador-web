f630593f0f6b54e3867f0dfbfe7bf682dcab34c8
Marco Reis <ma@marcoreis.net>
2016-06-28 16:07:59 -0300

    update

--- src/main/java/net/visualizacao/apresentacao/BuscaLivreBean.java
+++ src/main/java/net/visualizacao/apresentacao/BuscaLivreBean.java
@@ -30,239 +30,239 @@ import net.visualizacao.util.UtilBusca;
 @ManagedBean
 @ViewScoped
 public class BuscaLivreBean extends BaseBean {
-    private static final int QUANTIDADE_CARACTERES_VISUALIZACAO = 400;
-    private static final long serialVersionUID = -7508553590263034662L;
-    private String consulta;
-    private long duracaoBusca;
-    private ScoreDoc[] itens;
-    private int idFonteDados;
-    private FonteDados fonte;
-    private Document documento;
-    private UtilBusca buscador;
-    private Map<Integer, Document> mapaItens;
-    private Collection<Document> listaDocumentos;
-    private int totalHits;
+	private static final int QUANTIDADE_CARACTERES_VISUALIZACAO = 400;
+	private static final long serialVersionUID = -7508553590263034662L;
+	private String consulta;
+	private long duracaoBusca;
+	private ScoreDoc[] itens;
+	private int idFonteDados;
+	private FonteDados fonte;
+	private Document documento;
+	private UtilBusca buscador;
+	private Map<Integer, Document> mapaItens;
+	private Collection<Document> listaDocumentos;
+	private int totalHits;
 
-    public UtilBusca getBuscador() throws IOException {
-	if (buscador == null) {
-	    buscador = new UtilBusca(getFonte().getDiretorioIndice());
-	}
-	return buscador;
-    }
-
-    public void setConsulta(String consulta) {
-	this.consulta = consulta;
-    }
-
-    public String getConsulta() {
-	return consulta;
-    }
-
-    public BigDecimal getDuracaoBusca() {
-	Double d = duracaoBusca / 1000d;
-	BigDecimal bd = new BigDecimal(d).setScale(4, BigDecimal.ROUND_CEILING);
-	return bd;
-    }
-
-    public Collection<String> getCamposSelecionados() {
-	try {
-	    if (StringUtils.vazia(fonte.getDiretorio())) {
-		Collection<String> campos = new ArrayList<String>();
-		for (MetaDado metadado : fonte.getMetadados()) {
-		    campos.add(metadado.getCampo());
+	public UtilBusca getBuscador() throws IOException {
+		if (buscador == null) {
+			buscador = new UtilBusca(getFonte().getDiretorioIndice());
 		}
-		return campos;
-	    } else {
-		Collection<String> listaCamposArquivo = new ArrayList<String>();
-		listaCamposArquivo.add("Texto");
-		return listaCamposArquivo;
-	    }
-	} catch (Exception e) {
-	    // Consulta diretorio de documentos
-	    logger.error(e);
+		return buscador;
 	}
-	return null;
-    }
 
-    public void consultar() {
-	try {
-	    fonte = FachadaBuscador.getInstancia().buscarFontePeloId(getIdFonteDados());
-	    TopDocs hits = getBuscador().buscar(getConsulta());
-	    setItens(hits.scoreDocs);
-	    totalHits = hits.totalHits;
-	    if (totalHits == 0) {
-		setItens(null);
-	    }
-	    duracaoBusca = getBuscador().getDuracaoBusca();
-	    // } catch (NoSuchDirectoryException e) {
-	    // errorMsg("Essa fonte de dados ainda não foi indexada. Faça a
-	    // indexação no menu de Utilitário.");
-	} catch (Exception e) {
-	    errorMsg(e);
-	} finally {
-	    fecharIndice();
+	public void setConsulta(String consulta) {
+		this.consulta = consulta;
 	}
-    }
 
-    public int getTotalHits() {
-	return totalHits;
-    }
-
-    public void setItens(ScoreDoc[] itens) {
-	this.itens = itens;
-    }
-
-    public ScoreDoc[] getItens() {
-	return itens;
-    }
-
-    public Document doc(int doc) {
-	try {
-	    return getBuscador().doc(doc);
-	} catch (IOException e) {
-	    logger.error(e);
+	public String getConsulta() {
+		return consulta;
 	}
-	return null;
-    }
 
-    public String visualizarDetalhe(ScoreDoc scoreDoc) {
-	try {
-	    documento = doc(scoreDoc.doc);
-	} catch (Exception e) {
-	    logger.error(e);
+	public BigDecimal getDuracaoBusca() {
+		Double d = duracaoBusca / 1000d;
+		BigDecimal bd = new BigDecimal(d).setScale(4, BigDecimal.ROUND_CEILING);
+		return bd;
 	}
-	return "DetalheDocumento";
-    }
 
-    public String mostraDados(ScoreDoc doc) {
-	StringBuilder saida = new StringBuilder();
-	try {
-	    Document documento = doc(doc.doc);
-	    // Arquivo no disco
-	    if (fonte.getMetadados() == null || fonte.getMetadados().size() == 0) {
-		String id = documento.get("ID");
-		String resultado = id == null ? "" : "[" + id + "] - ";
-		resultado += limitaTamanho(documento.get("TextoCompleto.hl"));
-		return resultado;
-	    }
-	    // Registro do banco
-	    for (MetaDado metadado : fonte.getMetadados()) {
-		String campo = metadado.getCampo().toUpperCase();
-		String conteudo = documento.get(campo);
-		conteudo = limitaTamanho(conteudo);
-		saida.append("[");
-		saida.append(campo);
-		saida.append(": ");
-		saida.append(conteudo);
-		saida.append("] ");
-	    }
-	} catch (Exception e) {
-	    saida.append("Erro: " + e);
+	public Collection<String> getCamposSelecionados() {
+		try {
+			if (StringUtils.vazia(fonte.getDiretorio())) {
+				Collection<String> campos = new ArrayList<String>();
+				for (MetaDado metadado : fonte.getMetadados()) {
+					campos.add(metadado.getCampo());
+				}
+				return campos;
+			} else {
+				Collection<String> listaCamposArquivo = new ArrayList<String>();
+				listaCamposArquivo.add("Texto");
+				return listaCamposArquivo;
+			}
+		} catch (Exception e) {
+			// Consulta diretorio de documentos
+			logger.error(e);
+		}
+		return null;
 	}
-	return saida.toString();
-    }
 
-    public String mostraDadosPorID(int doc) {
-	StringBuilder saida = new StringBuilder();
-	Document documento = mapaItens.get(doc);
-	try {
-	    // Arquivo no disco
-	    if (fonte.getMetadados() == null || fonte.getMetadados().size() == 0) {
-		String id = documento.get("ID");
-		String resultado = id == null ? "" : "[" + id + "] - ";
-		resultado += limitaTamanho(documento.get("TextoCompleto.hl"));
-		return resultado;
-	    }
-	    // Registro do banco
-	    for (MetaDado metadado : fonte.getMetadados()) {
-		String campo = metadado.getCampo().toUpperCase();
-		String conteudo = documento.get(campo);
-		conteudo = limitaTamanho(conteudo);
-		saida.append("[");
-		saida.append(campo);
-		saida.append(": ");
-		saida.append(conteudo);
-		saida.append("] ");
-	    }
-	} catch (Exception e) {
-	    saida.append("Erro: " + e);
+	public void consultar() {
+		try {
+			fonte = FachadaBuscador.getInstancia().buscarFontePeloId(getIdFonteDados());
+			TopDocs hits = getBuscador().buscar(getConsulta());
+			setItens(hits.scoreDocs);
+			totalHits = hits.totalHits;
+			if (totalHits == 0) {
+				setItens(null);
+			}
+			duracaoBusca = getBuscador().getDuracaoBusca();
+			// } catch (NoSuchDirectoryException e) {
+			// errorMsg("Essa fonte de dados ainda não foi indexada. Faça a
+			// indexação no menu de Utilitário.");
+		} catch (Exception e) {
+			errorMsg(e);
+		} finally {
+			fecharIndice();
+		}
 	}
-	return saida.toString();
-    }
 
-    private String limitaTamanho(String conteudo) {
-	if (conteudo != null && conteudo.length() > QUANTIDADE_CARACTERES_VISUALIZACAO) {
-	    conteudo = conteudo.substring(0, QUANTIDADE_CARACTERES_VISUALIZACAO) + " (...)";
+	public int getTotalHits() {
+		return totalHits;
 	}
-	return conteudo;
-    }
 
-    public void setIdFonteDados(int idFonteDados) {
-	this.idFonteDados = idFonteDados;
-    }
-
-    public int getIdFonteDados() {
-	return idFonteDados;
-    }
-
-    public FonteDados getFonte() {
-	return fonte;
-    }
-
-    public Document getDocumento() {
-	return documento;
-    }
-
-    public String getDocumentoFormatado() {
-	return documento.get("TextoCompleto").replaceAll("\n", "<br />").replaceAll("�", "");
-    }
-
-    public String abrirPaginaBusca() {
-	return "consultar";
-    }
-
-    public static void main(String[] args) {
-	Pattern p = Pattern.compile("doc.(\\d*).*");
-	Matcher matcher = p.matcher("doc=1052 score=0.43030652");
-	// System.out.println(matcher.find());
-	System.out.println(matcher.group(1));
-    }
-
-    public boolean caminhoArquivo(String valor) {
-	return new File(valor).exists();
-    }
-
-    public void download(String fileName) {
-	try {
-	    HttpServletResponse response = null;// getResponse();
-	    File arquivo = new File(fileName);
-	    response.addHeader("Content-Disposition:", "attachment; filename=" + arquivo.getName());
-	    OutputStream out = response.getOutputStream();
-	    InputStream input = new FileInputStream(fileName);
-	    IOUtils.copy(input, out);
-	    out.flush();
-	    out.close();
-	    // getFacesContext().responseComplete();
-	} catch (Exception e) {
-	    // errorMsg("erro.generico",
-	    // "Nï¿½o foi possï¿½vel baixar o arquivo. Erro Java: " + e);
-	    logger.error(e);
+	public void setItens(ScoreDoc[] itens) {
+		this.itens = itens;
 	}
-    }
 
-    public String formataTexto(String texto) {
-	return texto.replaceAll("\n", "<br />");
-    }
-
-    public void fecharIndice() {
-	try {
-	    getBuscador().fechar();
-	} catch (Exception e) {
-	    throw new RuntimeException(e);
+	public ScoreDoc[] getItens() {
+		return itens;
 	}
-    }
 
-    public Collection<Document> getListaDocumentos() {
-	return listaDocumentos;
-    }
+	public Document doc(int doc) {
+		try {
+			return getBuscador().doc(doc);
+		} catch (IOException e) {
+			logger.error(e);
+		}
+		return null;
+	}
+
+	public String visualizarDetalhe(ScoreDoc scoreDoc) {
+		try {
+			documento = doc(scoreDoc.doc);
+		} catch (Exception e) {
+			logger.error(e);
+		}
+		return "DetalheDocumento";
+	}
+
+	public String mostraDados(ScoreDoc doc) {
+		StringBuilder saida = new StringBuilder();
+		try {
+			Document documento = doc(doc.doc);
+			// Arquivo no disco
+			if (fonte.getMetadados() == null || fonte.getMetadados().size() == 0) {
+				String id = documento.get("ID");
+				String resultado = id == null ? "" : "[" + id + "] - ";
+				resultado += limitaTamanho(documento.get("TextoCompleto.hl"));
+				return resultado;
+			}
+			// Registro do banco
+			for (MetaDado metadado : fonte.getMetadados()) {
+				String campo = metadado.getCampo().toUpperCase();
+				String conteudo = documento.get(campo);
+				conteudo = limitaTamanho(conteudo);
+				saida.append("[");
+				saida.append(campo);
+				saida.append(": ");
+				saida.append(conteudo);
+				saida.append("] ");
+			}
+		} catch (Exception e) {
+			saida.append("Erro: " + e);
+		}
+		return saida.toString();
+	}
+
+	public String mostraDadosPorID(int doc) {
+		StringBuilder saida = new StringBuilder();
+		Document documento = mapaItens.get(doc);
+		try {
+			// Arquivo no disco
+			if (fonte.getMetadados() == null || fonte.getMetadados().size() == 0) {
+				String id = documento.get("ID");
+				String resultado = id == null ? "" : "[" + id + "] - ";
+				resultado += limitaTamanho(documento.get("TextoCompleto.hl"));
+				return resultado;
+			}
+			// Registro do banco
+			for (MetaDado metadado : fonte.getMetadados()) {
+				String campo = metadado.getCampo().toUpperCase();
+				String conteudo = documento.get(campo);
+				conteudo = limitaTamanho(conteudo);
+				saida.append("[");
+				saida.append(campo);
+				saida.append(": ");
+				saida.append(conteudo);
+				saida.append("] ");
+			}
+		} catch (Exception e) {
+			saida.append("Erro: " + e);
+		}
+		return saida.toString();
+	}
+
+	private String limitaTamanho(String conteudo) {
+		if (conteudo != null && conteudo.length() > QUANTIDADE_CARACTERES_VISUALIZACAO) {
+			conteudo = conteudo.substring(0, QUANTIDADE_CARACTERES_VISUALIZACAO) + " (...)";
+		}
+		return conteudo;
+	}
+
+	public void setIdFonteDados(int idFonteDados) {
+		this.idFonteDados = idFonteDados;
+	}
+
+	public int getIdFonteDados() {
+		return idFonteDados;
+	}
+
+	public FonteDados getFonte() {
+		return fonte;
+	}
+
+	public Document getDocumento() {
+		return documento;
+	}
+
+	public String getDocumentoFormatado() {
+		return documento.get("TextoCompleto").replaceAll("\n", "<br />").replaceAll("�", "");
+	}
+
+	public String abrirPaginaBusca() {
+		return "consultar";
+	}
+
+	public static void main(String[] args) {
+		Pattern p = Pattern.compile("doc.(\\d*).*");
+		Matcher matcher = p.matcher("doc=1052 score=0.43030652");
+		// System.out.println(matcher.find());
+		System.out.println(matcher.group(1));
+	}
+
+	public boolean caminhoArquivo(String valor) {
+		return new File(valor).exists();
+	}
+
+	public void download(String fileName) {
+		try {
+			HttpServletResponse response = null;// getResponse();
+			File arquivo = new File(fileName);
+			response.addHeader("Content-Disposition:", "attachment; filename=" + arquivo.getName());
+			OutputStream out = response.getOutputStream();
+			InputStream input = new FileInputStream(fileName);
+			IOUtils.copy(input, out);
+			out.flush();
+			out.close();
+			// getFacesContext().responseComplete();
+		} catch (Exception e) {
+			// errorMsg("erro.generico",
+			// "Nï¿½o foi possï¿½vel baixar o arquivo. Erro Java: " + e);
+			logger.error(e);
+		}
+	}
+
+	public String formataTexto(String texto) {
+		return texto.replaceAll("\n", "<br />");
+	}
+
+	public void fecharIndice() {
+		try {
+			getBuscador().fechar();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public Collection<Document> getListaDocumentos() {
+		return listaDocumentos;
+	}
 }
--- src/main/java/net/visualizacao/util/UtilBusca.java
+++ src/main/java/net/visualizacao/util/UtilBusca.java
@@ -28,183 +28,183 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.NIOFSDirectory;
 
 public class UtilBusca {
-    private static Logger logger = Logger.getLogger(UtilBusca.class);
-    private FSDirectory diretorio;
-    private IndexSearcher buscador;
-    // private IndexReader reader;
-    private long duracaoBusca;
-    private Integer quantidadeLimiteRegistros = 1000;
-    private String diretorioDicionarios;
-    private HashMap<String, String> args;
-    private Query query;
-    private Analyzer analyzer;
-    private SearcherManager sm;
+	private static Logger logger = Logger.getLogger(UtilBusca.class);
+	private FSDirectory diretorio;
+	private IndexSearcher buscador;
+	// private IndexReader reader;
+	private long duracaoBusca;
+	private Integer quantidadeLimiteRegistros = 1000;
+	private String diretorioDicionarios;
+	private HashMap<String, String> args;
+	private Query query;
+	private Analyzer analyzer;
+	private SearcherManager sm;
 
-    public Analyzer getAnalyzer() {
-	if (analyzer == null) {
-	    logger.info("Novo analyzer");
-	    args = new HashMap<String, String>();
-	    args.put("stopWords", "stopwords.txt");
-	    args.put("dictionaries", "synonyms.txt");
-	    args.put("baseDirectory", diretorioDicionarios);
-	    // args.put("luceneMatchVersion", Version.LUCENE_44.toString());
-	    // analyzer = new TimbreAnalyzer(Version.LUCENE_44, args);
-	    analyzer = new StandardAnalyzer();
+	public Analyzer getAnalyzer() {
+		if (analyzer == null) {
+			logger.info("Novo analyzer");
+			args = new HashMap<String, String>();
+			args.put("stopWords", "stopwords.txt");
+			args.put("dictionaries", "synonyms.txt");
+			args.put("baseDirectory", diretorioDicionarios);
+			// args.put("luceneMatchVersion", Version.LUCENE_44.toString());
+			// analyzer = new TimbreAnalyzer(Version.LUCENE_44, args);
+			analyzer = new StandardAnalyzer();
+		}
+		return analyzer;
 	}
-	return analyzer;
-    }
 
-    // public void reopen() throws IOException {
-    // if (reader == null) {
-    // diretorio = FSDirectory.open(new File(diretorioIndice));
-    // reader = DirectoryReader.open(diretorio);
-    // buscador = new IndexSearcher(reader);
-    // }
-    // }
+	// public void reopen() throws IOException {
+	// if (reader == null) {
+	// diretorio = FSDirectory.open(new File(diretorioIndice));
+	// reader = DirectoryReader.open(diretorio);
+	// buscador = new IndexSearcher(reader);
+	// }
+	// }
 
-    public UtilBusca(String diretorioIndice) throws IOException {
-	diretorio = new NIOFSDirectory(Paths.get(diretorioIndice));
-	this.diretorioDicionarios = diretorioIndice + "/dicionarios";
-	sm = new SearcherManager(diretorio, null);
-	buscador = sm.acquire();
-	logger.info("Abrir");
-    }
-
-    // public UtilBusca(String diretorioIndice) throws IOException {
-    // this.diretorioIndice = diretorioIndice;
-    // this.diretorioDicionarios = diretorioIndice + "/dicionarios";
-    // reopen();
-    // }
-
-    public void fechar() {
-	try {
-	    if (diretorio != null)
-		diretorio.close();
-	    diretorio = null;
-	} catch (Exception e) {
-	    logger.error(e);
+	public UtilBusca(String diretorioIndice) throws IOException {
+		diretorio = new NIOFSDirectory(Paths.get(diretorioIndice));
+		this.diretorioDicionarios = diretorioIndice + "/dicionarios";
+		sm = new SearcherManager(diretorio, null);
+		buscador = sm.acquire();
+		logger.info("Abrir");
 	}
-	try {
-	    sm.release(buscador);
-	    buscador = null;
-	    logger.info("Fechar");
-	    // reader.close();
-	    // reader = null;
-	} catch (Exception e) {
-	    logger.error(e);
-	}
-    }
 
-    public IndexSearcher getBuscador() throws IOException {
-	if (buscador == null) {
-	    logger.info("Abrir");
-	    buscador = sm.acquire();
-	}
-	return buscador;
-    }
-
-    public Document doc(int docID) throws IOException {
+	// public UtilBusca(String diretorioIndice) throws IOException {
+	// this.diretorioIndice = diretorioIndice;
+	// this.diretorioDicionarios = diretorioIndice + "/dicionarios";
 	// reopen();
-	// Formatter formatter = new SimpleHTMLFormatter("<strong>",
-	// "</strong>");
-	// Scorer scorer = new QueryScorer(getQuery());
-	FastVectorHighlighter fhl = new FastVectorHighlighter();
-	FieldQuery fq = fhl.getFieldQuery(getQuery());
-	Document doc = getBuscador().doc(docID);
-	String frag = fhl.getBestFragment(fq, getBuscador().getIndexReader(), docID, "TextoCompleto", 400);
-	if (frag == null)
-	    frag = "";
-	// Highlighter hl = new Highlighter(formatter, scorer);
-	// String texto = doc.get("TextoCompleto");
-	try {
-	    // TokenStream token = TokenSources.getTokenStream(doc,
-	    // "TextoCompleto", getAnalyzer());
-	    // String fragmentos = hl.getBestFragment(new StandardAnalyzer(
-	    // Version.LUCENE_44), "TextoCompleto", texto);
-	    // doc.add(new StringField("TextoCompleto.hl", fragmentos,
-	    // Store.NO));
-	    // String fragmentos = hl.getBestFragment(getAnalyzer(),
-	    // "TextoCompleto", texto);
-	    doc.add(new StringField("TextoCompleto.hl", frag, Store.NO));
-	} catch (Exception e) {
-	    logger.error(e);
-	} finally {
-	    fechar();
+	// }
+
+	public void fechar() {
+		try {
+			if (diretorio != null)
+				diretorio.close();
+			diretorio = null;
+		} catch (Exception e) {
+			logger.error(e);
+		}
+		try {
+			sm.release(buscador);
+			buscador = null;
+			logger.info("Fechar");
+			// reader.close();
+			// reader = null;
+		} catch (Exception e) {
+			logger.error(e);
+		}
 	}
-	return doc;
-    }
 
-    public Query getQuery() {
-	return query;
-    }
-
-    // public float getIdf(String termo, String campo) throws IOException {
-    // Term term = new Term(campo, termo);
-    // IDFExplanation explicacao = Similarity.getDefault().idfExplain(term,
-    // buscador);
-    // return explicacao.getIdf();
-    // }
-    // public double getTfIdf(String termo) {
-    // return Math.sqrt(buscador.maxDoc());
-    // }
-    public static void main(String[] args) {
-	try {
-	    UtilBusca buscador = null;
-	    long time = System.currentTimeMillis();
-	    String query = "PERANTE AUTORIDADE POLICIAL";
-	    TopDocs resultado = null; // buscador.busca("*", query);
-	    long total = System.currentTimeMillis() - time;
-	    // System.out.println(new BigDecimal(total / 1000d));
-	    System.out.println(resultado.totalHits);
-	} catch (Exception e) {
-	    e.printStackTrace();
+	public IndexSearcher getBuscador() throws IOException {
+		if (buscador == null) {
+			logger.info("Abrir");
+			buscador = sm.acquire();
+		}
+		return buscador;
 	}
-    }
 
-    // FIXME Desacoplar do Lucene (nao sei como).
-    public TopDocs processaSimilaridade(String campo, String[] termos) throws IOException {
-	// An easier way might be to submit doc A as a query
-	// (adding all words to the query as OR terms,
-	// boosting each by term frequency) and look
-	// for doc B in the result set.
-	BooleanQuery query = new BooleanQuery();
-	for (String termo : termos) {
-	    Term term = new Term(campo, termo);
-	    // TermQuery tQuery = new TermQuery(term);
-	    // tQuery.setBoost(getIdf(termo));
-	    // FuzzyQuery fQuery = new FuzzyQuery(term, 0.8f);
-	    FuzzyQuery fQuery = new FuzzyQuery(term);
-	    query.add(fQuery, Occur.SHOULD);
+	public Document doc(int docID) throws IOException {
+		// reopen();
+		// Formatter formatter = new SimpleHTMLFormatter("<strong>",
+		// "</strong>");
+		// Scorer scorer = new QueryScorer(getQuery());
+		FastVectorHighlighter fhl = new FastVectorHighlighter();
+		FieldQuery fq = fhl.getFieldQuery(getQuery());
+		Document doc = getBuscador().doc(docID);
+		String frag = fhl.getBestFragment(fq, getBuscador().getIndexReader(), docID, "TextoCompleto", 400);
+		if (frag == null)
+			frag = "";
+		// Highlighter hl = new Highlighter(formatter, scorer);
+		// String texto = doc.get("TextoCompleto");
+		try {
+			// TokenStream token = TokenSources.getTokenStream(doc,
+			// "TextoCompleto", getAnalyzer());
+			// String fragmentos = hl.getBestFragment(new StandardAnalyzer(
+			// Version.LUCENE_44), "TextoCompleto", texto);
+			// doc.add(new StringField("TextoCompleto.hl", fragmentos,
+			// Store.NO));
+			// String fragmentos = hl.getBestFragment(getAnalyzer(),
+			// "TextoCompleto", texto);
+			doc.add(new StringField("TextoCompleto.hl", frag, Store.NO));
+		} catch (Exception e) {
+			logger.error(e);
+		} finally {
+			fechar();
+		}
+		return doc;
 	}
-	TopDocs hits = getBuscador().search(query, quantidadeLimiteRegistros);
-	return hits;
-    }
 
-    public long getDuracaoBusca() {
-	return duracaoBusca;
-    }
-
-    public TopDocs buscar(String consulta) throws ParseException {
-	consulta = Normalizer.normalize(consulta, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+",
-		"");
-	QueryParser queryParser = new QueryParser("", getAnalyzer());
-	queryParser.setDefaultOperator(Operator.AND);
-	query = queryParser.parse("TextoCompleto:(" + consulta + ")");
-	return buscar();
-    }
-
-    public TopDocs buscar() {
-	TopDocs hits;
-	try {
-	    long time = System.currentTimeMillis();
-	    hits = getBuscador().search(query, quantidadeLimiteRegistros);
-	    duracaoBusca = System.currentTimeMillis() - time;
-	} catch (Exception e) {
-	    throw new RuntimeException(e);
-	} finally {
-	    // fechar();
+	public Query getQuery() {
+		return query;
 	}
-	return hits;
-    }
+
+	// public float getIdf(String termo, String campo) throws IOException {
+	// Term term = new Term(campo, termo);
+	// IDFExplanation explicacao = Similarity.getDefault().idfExplain(term,
+	// buscador);
+	// return explicacao.getIdf();
+	// }
+	// public double getTfIdf(String termo) {
+	// return Math.sqrt(buscador.maxDoc());
+	// }
+	public static void main(String[] args) {
+		try {
+			UtilBusca buscador = null;
+			long time = System.currentTimeMillis();
+			String query = "PERANTE AUTORIDADE POLICIAL";
+			TopDocs resultado = null; // buscador.busca("*", query);
+			long total = System.currentTimeMillis() - time;
+			// System.out.println(new BigDecimal(total / 1000d));
+			System.out.println(resultado.totalHits);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// FIXME Desacoplar do Lucene (nao sei como).
+	public TopDocs processaSimilaridade(String campo, String[] termos) throws IOException {
+		// An easier way might be to submit doc A as a query
+		// (adding all words to the query as OR terms,
+		// boosting each by term frequency) and look
+		// for doc B in the result set.
+		BooleanQuery query = null;// new BooleanQuery();
+		for (String termo : termos) {
+			Term term = new Term(campo, termo);
+			// TermQuery tQuery = new TermQuery(term);
+			// tQuery.setBoost(getIdf(termo));
+			// FuzzyQuery fQuery = new FuzzyQuery(term, 0.8f);
+			FuzzyQuery fQuery = new FuzzyQuery(term);
+//			query.add(fQuery, Occur.SHOULD);
+		}
+		TopDocs hits = getBuscador().search(query, quantidadeLimiteRegistros);
+		return hits;
+	}
+
+	public long getDuracaoBusca() {
+		return duracaoBusca;
+	}
+
+	public TopDocs buscar(String consulta) throws ParseException {
+		consulta = Normalizer.normalize(consulta, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+",
+				"");
+		QueryParser queryParser = new QueryParser("", getAnalyzer());
+		queryParser.setDefaultOperator(Operator.AND);
+		query = queryParser.parse("TextoCompleto:(" + consulta + ")");
+		return buscar();
+	}
+
+	public TopDocs buscar() {
+		TopDocs hits;
+		try {
+			long time = System.currentTimeMillis();
+			hits = getBuscador().search(query, quantidadeLimiteRegistros);
+			duracaoBusca = System.currentTimeMillis() - time;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			// fechar();
+		}
+		return hits;
+	}
 
 }
